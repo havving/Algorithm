c11231d5f5b686d7821757b567e990769e166671
HAVVING <sparkle5406@gmail.com>
2021-08-11 22:30:10 +0900

    0811 al 1655

--- src/test/java/org/example/August_2.java
+++ src/test/java/org/example/August_2.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.Test;
 
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class August_2 {
@@ -43,4 +44,32 @@ public class August_2 {
     }
 
 
+    @Test
+    public void al_1655() {
+        Scanner scan = new Scanner(System.in);
+
+        // 우선순위 큐
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        int n = scan.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            int num = scan.nextInt();
+
+            // 최대/최소 우선순위 큐의 크기가 같으면, 최대 큐에 숫자 입력
+            if (minHeap.size() == maxHeap.size()) maxHeap.add(num);
+            else minHeap.add(num);
+
+            // 최대/최소 우선순위 큐가 비어있지 않고, 최대 큐의 루트값이 더 크면 최소 큐의 루트값과 자리 바꿈
+            if (!minHeap.isEmpty() && !maxHeap.isEmpty() && minHeap.peek() < maxHeap.peek()) {
+                int temp = minHeap.poll();
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(temp);
+            }
+            sb.append(maxHeap.peek() + "\n");
+        }
+        System.out.println(sb);
+    }
 }
