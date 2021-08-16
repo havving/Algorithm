7574f099c9084dd164c5135df248105bc4a56f45
HAVVING <sparkle5406@gmail.com>
2021-08-16 21:01:32 +0900

    0816 al 15900

--- src/test/java/org/example/August_2.java
+++ src/test/java/org/example/August_2.java
@@ -18,6 +18,9 @@ public class August_2 {
     public static boolean[] v;
     public static ArrayList<Integer>[] n_list;
 
+    static LinkedList<Integer>[] adjList;
+    public static int answer = 0;
+
 
     @Test
     public void al_12865() {
@@ -224,6 +227,48 @@ public class August_2 {
                 dfs(next, depth+1);
         }
         v[start] = false;
+    }
+
+
+    @Test
+    public void al_15900() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        adjList = new LinkedList[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new LinkedList<Integer>();
+        }
+
+        for(int i = 0; i < N-1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adjList[a].add(b);
+            adjList[b].add(a);
+        }
+
+        dfs(1, 0, 0);
+
+        System.out.println((answer % 2) == 0 ? "No" : "Yes");
+
+        br.close();
+    }
+
+    public static void dfs(int cur, int p, int cnt) {
+        for(int next : adjList[cur]) {
+            if(next != p) {
+                dfs(next, cur, cnt+1);
+            }
+        }
+
+        if(adjList[cur].size() == 1) {
+            answer += cnt;
+        }
     }
 
 }
